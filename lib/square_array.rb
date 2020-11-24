b489df73604795bb73ecb0ec42acf5421319acd8
menachem m margolis <margolismendel5728@gmail.com>
2020-11-24 18:08:51 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,8 @@
 def square_array(numbers,new_numbers)
   c=0
+  new_numbers=[]
  while c < numbers.length do
-  puts  numbers[c]=numbers[c]**2
+  puts  new_numbers.push(numbers[c]=numbers[c]**2)
   c+=1
  end
 end
